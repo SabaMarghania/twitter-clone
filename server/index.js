448ebdb6831406cf8f243d149976b6a91a5bd83a
saba <saba.marghania@gmail.com>
2021-09-10 17:01:01 +0400

    update

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ app.use(express.json())
 app.use(cors())
 const userRoutes = require("./routes/userRoute");
 const dotenv = require('dotenv')
+const path = require('path')
 //conecction
 dotenv.config();
 
@@ -18,7 +19,14 @@ mongoose.connect("mongodb+srv://saba:test123@cluster0.t4pyn.mongodb.net/twitterd
 })
 
 app.use("/", userRoutes);
+//production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('../client/build'));
 
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 app.listen(3001,()=>{
     console.log(`server is working on 3001 port`)
